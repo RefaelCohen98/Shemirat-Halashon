b81a91fb4f9f322d6a05f3ea1dac0a0bfab3e42d
Refael Cohen <refaelcohen98@gmail.com>
2024-03-14 22:40:59 +0200

    initial client

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React , { useEffect, useState }from 'react'
+
+const URL = "https://www.kolhalashon.com/New/Shiurim.aspx?Lang=Hebrew&RavID=-1&ROrder=-1&FID=67446&HideMenus=False"
+
+function ShemiratHalashon() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(URL, {mode: 'no-cors'})
+      console.log(result);
+    }
+    fetchData()
+  }, [])
+  
+  return (
+    <div>Shemirat Halashon!</div>
+  )
+}
+
+export default ShemiratHalashon
